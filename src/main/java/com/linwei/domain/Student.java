f1fd268d83584b1c84c3437549aa4e9ce1e9be3f
lw <lw@linwei.com>
2021-02-28 19:46:07 +0800

    123 Commit Message 必须填写提交信息~

---
+++ src/main/java/com/linwei/domain/Student.java
@@ -0,0 +1,12 @@
+package com.linwei.domain;
+
+/**
+ * @date 2021/2/28 19:34
+ */
+public class Student {
+
+    //属性私有化
+    private String sid;
+    private String sname;
+    private Integer age;
+}
