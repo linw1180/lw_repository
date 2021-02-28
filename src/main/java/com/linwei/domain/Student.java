1761ffc599548120bb4dd49dedaa5598b23afe0b
lw <lw@linwei.com>
2021-02-28 19:57:17 +0800

    二次提交~

--- src/main/java/com/linwei/domain/Student.java
+++ src/main/java/com/linwei/domain/Student.java
@@ -9,4 +9,28 @@ public class Student {
     private String sid;
     private String sname;
     private Integer age;
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
